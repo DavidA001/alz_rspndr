e484e89a593d9a3bc812e892aab250aa46f3a60a
David Argueta <david.argueta@annkissam.com>
2016-06-04 17:02:59 -0400

    Added hacky welcome and alert with audio.

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,38 @@
+var playAudio = false;
+
+$(function() {
+  hideAlertPartial();
+});
+
+function playAud() {
+  playAudio = !playAudio;
+
+  if(playAudio == true) {
+    hideWelcomePartial();
+    showAlertPartial();
+    $(".audio-play")[0].currentTime = 0;
+    alert("Warning!")
+    return $(".audio-play")[0].play();
+  } else {
+    hideAlertPartial();
+    showWelcomePartial();
+    alert("All good!")
+    return $(".audio-play")[0].pause();
+  }
+}
+
+function hideWelcomePartial() {
+  $(".welcome").hide();
+}
+
+function showWelcomePartial() {
+  $(".welcome").show();
+}
+
+function hideAlertPartial() {
+  $(".alert").hide();
+}
+
+function showAlertPartial() {
+  $(".alert").show();
+}
